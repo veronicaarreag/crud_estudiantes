80a05546299dfec230b52844fb10f3979816b53e
veronicaarreag <102401074+veronicaarreag@users.noreply.github.com>
2022-05-03 23:31:26 -0600

    Update Beronica.cpp

--- Beronica/Beronica.cpp
+++ Beronica/Beronica.cpp
@@ -7,7 +7,11 @@ int main(array<System::String ^> ^args)
 {
     Database^ data = gcnew Database();
     DateTime date = DateTime::Now;
-    //data->insertar(2, "0901", "Vanesa", "Hernandez", "11 Av", 59621085, true, "olstertecn", date);
+    data->insertar(2, "0901", "Vanesa", "Hernandez", "11 Av", 59621085, true, "vane_@gmail.com", date);
+    data->consulta();
+    data->editar("Vanesa", "10107", "Oscar", "Juarez", "15 Av", 69854522, false, "oscar_51@gmail.com", date);
+    data->consulta();
+    data->eliminar("Oscar");
     data->consulta();
     return 0;
 }
